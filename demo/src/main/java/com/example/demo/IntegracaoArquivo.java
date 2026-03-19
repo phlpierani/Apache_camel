bb6be2a450260748bb00104da2c5a6eb7c96e4cb
phlpierani <phl.pierani@gmail.com>
2026-03-18 22:55:17 -0300

    Update Apache Camel configuration and enhance file processing route

--- demo/src/main/java/com/example/demo/IntegracaoArquivo.java
+++ demo/src/main/java/com/example/demo/IntegracaoArquivo.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileConstants;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,9 +9,12 @@ public class IntegracaoArquivo extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:C:/Users/pedro/Downloads/entrada apache camel")
-                .routeId("integracao-arquivo")
-                .to("file:C:/Users/pedro/Downloads/saida apache camel");
+        from("file:{{diretorioEntrada}}?delay=5000") // especificado no application.properties, o delay é o tempo de espera entre as verificações do diretório de entrada
+                .routeId("integracao-arquivo")           // define um ID para a rota, que pode ser usado para monitoramento e gerenciamento
+                .log("Processando o arquivo: ${file:name}")  // loga o nome do arquivo que está sendo processado
+                .setHeader(FileConstants.FILE_NAME).simple("${date:now:HHmmss}_${file:name}") // renomeia o arquivo adicionando um timestamp ao nome original
+                .to("file:{{diretorioSaida}}");         // especificado no application.properties
+
         // O código acima define uma rota do Apache Camel que lê arquivos de um diretório de entrada e os move para um diretório de saída.
         // A rota é identificada pelo ID "integracao-arquivo".
     }
