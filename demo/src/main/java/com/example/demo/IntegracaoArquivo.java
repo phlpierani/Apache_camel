36a7533cf2c71917c08c604d66b3084c7300e23c
phlpierani <phl.pierani@gmail.com>
2026-08-01 21:56:35 -0300

    Choice, Throttle e HTTP

--- demo/src/main/java/com/example/demo/IntegracaoArquivo.java
+++ demo/src/main/java/com/example/demo/IntegracaoArquivo.java
@@ -1,21 +1,34 @@
 package com.example.demo;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpConstants;
+import org.apache.camel.support.builder.Namespaces;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class IntegracaoArquivo extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:{{diretorioEntrada}}?delay=5000")
+        var ns = new Namespaces("ns", "https://www.portalfiscal.inf.br/nfe" );
+
+        from("file:{{diretorioEntrada}}?delay=5000&include=.*.xml")
                 .routeId("integracao-arquivo")
                 .log("Processando o arquivo: ${file:name}")
+                .setProperty("CNPJ",xpath("{{xpathCnpjTransportadora}}", String.class, ns))
+                .log("CNPJ extraído: ${exchangeProperty.CNPJ}")
                 .choice()
-                .when(xpath("{{xpathCnpjTransportadora}}").isEqualTo("1"))
+                .when(exchangeProperty("CNPJ").isEqualTo("1"))
                 .to("file:{{diretorioSaida}}?fileName=${date:now:HHmmss}_${file:name}")
-                .when(xpath("{{xpathCnpjTransportadora}}").isEqualTo("2"))
-                .log("HTTP")
+                .when(exchangeProperty("CNPJ").isEqualTo("2"))
+                    .throttle(1).timePeriodMillis(5000)
+                        .setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
+                        .setHeader(HttpConstants.HTTP_URI, constant("{{UrlApiTransportadora2}}"))
+                        .setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
+                        .setHeader(HttpConstants.CONTENT_TYPE, constant("application/xml"))
+                        .to("http:servidorTransportadora2")
+                        .endChoice()
                 .otherwise()
                 .log("Transportadora nao integrada")
                 .end();
